173ab933dfc1e75683eab6e5e3c6c9282897574f
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-08-11 18:31:52 -0400

    Revised routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,75 +3,70 @@ const router = require('express').Router();
 const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-//show all blog posts
+// render Home (public)
 router.get('/', async (req, res) => {
   try {
-    req.body.user_id = req.session.user_id
-
-    // Get all blog posts
+    req.body.user_id = req.session.user_id;
     const blogData = await Blog.findAll({
-
+      include: [{
+        model: User,
+        attributes: {exclude: ['password']},
+      }],
+      order: [['created', 'DESC']]
     });
-    console.log(blogData)
-
     const blogMap = blogData.map((blog) => blog.get({ plain: true }));
-    console.log(blogData)
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      blogMap
-    });
+      res.render('homepage', {
+      blogMap,
+    })
   } catch (err) {
     console.log(err)
-    res.status(500).json(err);
-  }
-});
-
-// shows posts unique to user
-router.get('/userblog', async (req, res) => {
-  try {
-    // get all blog posts
-    const blogData = await Blog.findAll({
-      where: { user_id: req.session.user_id }
-
-    });
-    console.log(blogData)
-
-    const blogMap = blogData.map((blog) => workout.get({ plain: true }));
-    const userData = await Blog.findOne({
-      where: { user_id: req.session.user_id },
-      include: [
-        {
-          model: User,
-          attributes: ['id'],
-        },
-      ],
-    });
-
-    if (!userData) {
-      res.render('userblog', {
-        blogMap,
-        logged_in: req.session.logged_in
-      });
-
-    } else {
-
-      const userMap = userData.get({ plain: true });
-
-      res.render('userblog', {
-        blogMap, ...userMap,
-        logged_in: req.session.logged_in
-      });
+    res.status(500).json(err)
     }
+}); 
 
+// render /dashboard (unique to user posts)
+router.get('/dashboard', async (req, res) => {
+  try {
+    if (req.session.user_id) {
+      const blogData = await Blog.findAll({
+        where: {
+          user_id: req.session.user_id
+        },
+        include: [{
+          model: User,
+          attributes: {exclude: ['password']},
+        }],
+        order: [['created', 'DESC']]
+      });
+      const blogMap = blogData.map((blog) => blog.get({ plain: true }));
+      res.render('homepage', {
+        blogMap,
+        user_id: req.session.user_id
+      });
+      return;
+    } else {
+      res.redirect('/login')
+    }
+    
   } catch (err) {
     console.log(err)
-    res.status(500).json(err);
+    res.status(500).json(err)
+  }
+}); 
+
+// render /login
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+  } else {
+    res.render('login');
   }
 });
 
-// sign-up 
+// render sign-up 
 router.get('/signup', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+  // If the user is already logged in, redirect the request to /dashboard (unique to user posts)
   if (req.session.logged_in) {
     res.redirect('/dashboard');
   } else {
@@ -80,24 +75,45 @@ router.get('/signup', (req, res) => {
 });
 
 
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/dashboard');
-  } else {
-    res.render('login');
-  }
-});
-
-router.get('/dashboard', async (req, res) => {
-
-    if (req.session.logged_in) {
-      res.render('dashboard');
-    } else {
-      res.redirect('/login');
-    }
-});
-
-
-
 module.exports = router;
+
+
+
+
+
+// router.get('/', async (req, res) => {
+//   try {
+//     req.body.user_id = req.session.user_id
+//     // Get all blog posts
+//     const blogData = await Blog.findAll({
+
+//     });
+//     console.log(blogData)
+
+//     const blogMap = blogData.map((blog) => blog.get({ plain: true }));
+//     console.log(blogData)
+//     // Pass serialized data and session flag into template
+//     res.render('homepage', {
+//       blogMap
+//     });
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+
+
+
+// router.get('/dashboard', async (req, res) => {
+
+//     if (req.session.logged_in) {
+//       res.render('dashboard');
+//     } else {
+//       res.redirect('/login');
+//     }
+// });
+
+
--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes')
 const apiRoutes = require('./api')
-const dashboardRoutes = require('./api/dashboardRoutes')
 
 // localhost:3001/
 router.use('/', homeRoutes)
 
-// localhost:3001/dashboard
-router.use('/dashboard', dashboardRoutes)
-
 // localhost:3001/api
 router.use('/api', apiRoutes)
 
-// localhost:3001/api
-router.use('/userblog', homeRoutes)
+
+// // localhost:3001/dashboard
+// router.use('/dashboard', dashboardRoutes)
+
+// // localhost:3001/api
+// router.use('/signup', homeRoutes)
 
 
 module.exports = router;
